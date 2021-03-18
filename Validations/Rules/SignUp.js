8a4006156de8cfe94c46e0ba02e10f50c2db1b7e
Vishal Kumar <vishakumar@BLR.ALLEGISINDIA.COM>
2021-03-18 13:47:01 +0530

    changed phone number to string

--- Validations/Rules/SignUp.js
+++ Validations/Rules/SignUp.js
@@ -4,7 +4,7 @@ module.exports = {
     SignUpValidationRule: [ 
         body('email', 'Invalid email').exists().isEmail(),
         body('password').exists().isStrongPassword(),
-        body('phoneNumber').exists().isInt().isMobilePhone().isLength({ min: 10, max: 10}),
+        body('phoneNumber').exists().isString().isMobilePhone().isLength({ min: 10, max: 10}),
         body('firstName').exists().isString(),
         body('lastName').exists().isString(),
         body('userType').exists().isInt({ min: 1, max: 2})
