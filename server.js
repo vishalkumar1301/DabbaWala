493e3dee8a92c0fa86fcbe4bad3300f8ecc5db3e
vishal <vishalkumar1301@gmail.com>
2021-03-26 17:51:12 +0530

    added sample code for file upload

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const passport = require('passport');
+const multer = require('multer');
+const crypto = require('crypto');
+const path = require('path');
 
 // import custom modules
 const configuration = require('./config');
@@ -14,6 +17,17 @@ dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
+storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: function(req, file, cb) {
+      return crypto.pseudoRandomBytes(16, function(err, raw) {
+        if (err) {
+          return cb(err);
+        }
+        return cb(null, "" + (raw.toString('hex')) + (path.extname(file.originalname)));
+      });
+    }
+  });
 
 
 // middlewares
@@ -21,10 +35,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
+
+app.post("/upload", multer({
+        storage: storage
+    }).single('photo'), function(req, res) {
+        return res.status(200).json({message: req.file.filename});
+});
+
 app.use('/auth', authenticationRoutes);
 app.use('/address', addressRoute);
-
-// start sever
-app.listen(port, function() {
-    logger.info(`App running on port ${port}`)
+    
+    // start sever
+    app.listen(port, function() {
+        logger.info(`App running on port ${port}`)
 });
