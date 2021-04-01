154262134fc001de3d8e7989ae095a497deb5379
vishal <vishalkumar1301@gmail.com>
2021-04-01 22:23:28 +0530

    changed code to add address to check for concurrency issue

--- Routes/Address.js
+++ Routes/Address.js
@@ -16,19 +16,19 @@ addressRoute.post('/', AddressValidationRule, AddressValidationCheck, async (req
         if(err) {
             res.status(500).json(new JSONResponse(Constants.ErrorMessages.InternalServerError).getJson());
         };
+        // general check for all the address fields
+        var index = req.user.addresses.findIndex(x => x.city === req.body.city && x.state === req.body.state && x.pincode === req.body.pincode && x.address === req.body.address && x.landmark === req.body.landmark && x.streetOrBuildingName === req.body.streetOrBuildingName);
+        if(index < 0) {
+            User.findOneAndUpdate({ _id: req.user._id }, { $push: { addresses: { city: req.body.city, state: req.body.state, pincode: req.body.pincode, address: req.body.address, landmark: req.body.landmark, streetOrBuildingName: req.body.streetOrBuildingName, saveAs: req.body.saveAs, latitude: req.body.latitude, longitude: req.body.longitude, mapsAddress: req.body.mapsAddress, isSelected: true } } }, function (err, user) {
+                if(err) {
+                    res.status(500).json(new JSONResponse(Constants.ErrorMessages.InternalServerError).getJson());
+                };
+                res.json(new JSONResponse(null, Constants.SuccessMessages.AddressAddedSuccessfully).getJson());
+            });
+        } else {
+            res.status(409).json(new JSONResponse(null, Constants.SuccessMessages.AddressAlreadyExists).getJson());
+        }
     });
-    // general check for all the address fields
-    var index = req.user.addresses.findIndex(x => x.city === req.body.city && x.state === req.body.state && x.pincode === req.body.pincode && x.address === req.body.address && x.landmark === req.body.landmark && x.streetOrBuildingName === req.body.streetOrBuildingName);
-    if(index < 0) {
-        User.findOneAndUpdate({ _id: req.user._id }, { $push: { addresses: { city: req.body.city, state: req.body.state, pincode: req.body.pincode, address: req.body.address, landmark: req.body.landmark, streetOrBuildingName: req.body.streetOrBuildingName, saveAs: req.body.saveAs, latitude: req.body.latitude, longitude: req.body.longitude, mapsAddress: req.body.mapsAddress, isSelected: true } } }, function (err, user) {
-            if(err) {
-                res.status(500).json(new JSONResponse(Constants.ErrorMessages.InternalServerError).getJson());
-            };
-            res.json(new JSONResponse(null, Constants.SuccessMessages.AddressAddedSuccessfully).getJson());
-        });
-    } else {
-        res.status(409).json(new JSONResponse(null, Constants.SuccessMessages.AddressAlreadyExists).getJson());
-    }
 });
 
 addressRoute.post('/setdefaultaddress', ChangeDefaultAddress, AddressValidationCheck, async (req, res, next) => {
