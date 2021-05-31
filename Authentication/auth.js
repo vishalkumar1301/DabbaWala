a26120a2f8106a2f25dd339c11cc1850ae12ddbb
Vishal Kumar <vishakumar@BLR.ALLEGISINDIA.COM>
2021-05-31 16:36:06 +0530

    checking custom message in passport

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -4,13 +4,14 @@ const localStrategy = require('passport-local').Strategy;
 
 // custom modules
 const User = require('../Models/user');
+const { JSONResponse } = require('../Constants/Response');
 const { Constants } = require('../constants');
 
 module.exports = async function (passport) {
     passport.use(new localStrategy({usernameField: 'email'}, function (username, password, done) {
         User.findOne({email: username}, function (err, user) {
             if(err) return done(err);
-            if(!user) return done(null, false);
+            if(!user) return done(new JSONResponse(Constants.ErrorMessages.InternalServerError).getJson(), false);
             
             user.IsValidPassword(password, (err, isMatch)=>{
                 if(err) return done(err);
